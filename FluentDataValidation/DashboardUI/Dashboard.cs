4880af1df2505f48b2ee0471556ba80351099e6c
Ben Thornhill <bthornhill123@gmail.com>
2019-06-20 19:49:38 -0600

    Fluent data validation demo by Tim Corey.

--- FluentDataValidation/DashboardUI/Dashboard.cs
+++ FluentDataValidation/DashboardUI/Dashboard.cs
@@ -1,4 +1,6 @@
-﻿using ModelLibrary;
+﻿using DashboardUI.Validators;
+using FluentValidation.Results;
+using ModelLibrary;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -11,34 +13,49 @@ using System.Windows.Forms;
 
 namespace DashboardUI
 {
-  public partial class Dashboard : Form
-  {
-    BindingList<string> errors = new BindingList<string>();
-
-    public Dashboard()
+    public partial class Dashboard : Form
     {
-      InitializeComponent();
+        BindingList<string> errors = new BindingList<string>();
+
+        public Dashboard()
+        {
+            InitializeComponent();
+
+            errorListBox.DataSource = errors;
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            errors.Clear();
+
+            if (!decimal.TryParse(accountBalanceText.Text, out decimal accountBalance))
+            {
+                errors.Add("Account Balance: Invalid Amount");
+                return;
+            }
+
+            PersonModel person = new PersonModel();
+            person.FirstName = firstNameText.Text;
+            person.LastName = lastNameText.Text;
+            person.AccountBalance = accountBalance;
+            person.DateOfBirth = dateOfBirthPicker.Value;
+
+            // Validate my data
+            PersonValidator validator = new PersonValidator();
+
+            ValidationResult results = validator.Validate(person);
+
+            if (results.IsValid == false)
+            {
+                foreach (ValidationFailure failure in results.Errors )
+                {
+                    errors.Add(failure.ErrorMessage);
+                }
+            }
+
+            // Insert into the database
+
+            MessageBox.Show("Operation Complete");
+        }
     }
-
-    private void createButton_Click(object sender, EventArgs e)
-    {
-      errors.Clear();
-
-      if (!decimal.TryParse(accountBalanceText.Text, out decimal accountBalance))
-      {
-        errors.Add("Account Balance: Invalid Amount");
-        return;
-      }
-
-      PersonModel person = new PersonModel();
-      person.FirstName = firstNameText.Text;
-      person.LastName = lastNameText.Text;
-      person.AccountBalance = accountBalance;
-      person.DateOfBirth = dateOfBirthPicker.Value;
-
-      // Insert into the database
-
-      MessageBox.Show("Operation Complete");
-    }
-  }
 }
